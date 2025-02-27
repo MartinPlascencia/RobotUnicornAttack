86392a822940bd7a6b28847648e488210d292176
MartinPlascencia <martin.plascencia@live.com.mx>
2025-02-26 19:58:24 -0600

    Implemented Player Jump

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField]
+    private UnityEvent onGameStart;
+    void Start()
+    {
+        onGameStart?.Invoke();
+    }
+}
---
+++ Assets/Scripts/Jump.cs
@@ -0,0 +1,69 @@
+using UnityEngine;
+
+public class Jump : MonoBehaviour
+{
+    [SerializeField]
+    private float jumpForce = 5f;
+    [SerializeField]
+    private float maxJumpTime = 0.3f;
+    [SerializeField]
+    private float jumpBoost = 0.5f;
+    private Rigidbody rb;
+    private bool isGrounded;
+    private bool isJumping;
+    private float jumpTimeCounter;
+    private bool buttonPressed;
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    public void StartJump()
+    {
+        buttonPressed = true;
+        if (isGrounded)
+        {
+            isJumping = true;
+            jumpTimeCounter = maxJumpTime;
+            rb.linearVelocity = Vector3.up * jumpForce;
+            isGrounded = false;
+        }
+        Debug.Log("Jump");
+    }
+
+    public void EndJump()
+    {
+        buttonPressed = false;
+        isJumping = false;
+        Debug.Log("End Jump");
+    }
+
+    private void Update()
+    {
+        HandleJump();
+    }
+
+    private void HandleJump()
+    {
+        if (buttonPressed && isJumping)
+        {
+            if (jumpTimeCounter > 0)
+            {
+                rb.linearVelocity = Vector3.up * (jumpForce + jumpBoost);
+                jumpTimeCounter -= Time.deltaTime;
+            }
+            else
+            {
+                isJumping = false;
+            }
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+}
