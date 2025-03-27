e171fbc48a44d2ce5e180efc5e0f42048ce3fd07
MartinPlascencia <martin.plascencia@live.com.mx>
2025-03-26 19:55:53 -0600

    Implemented Effects, Changed Game Over Screen.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -10,12 +10,35 @@ public class GameManager : MonoBehaviour
     [SerializeField]
     private UnityEvent onFinishGame;
     [SerializeField]
+    private UnityEvent onLoseGame;
+    [SerializeField]
+    private UnityEvent onShowGameOverScreen;
+    [SerializeField]
     private float secondsToRestart = 3f;
     [SerializeField]
     private float finalSecondsToRestart = 5f;
+    [SerializeField]
+    private float secondsToShowGameOverScreen = 3f;
+
+    void Awake()
+    {
+        secondsToRestart+= secondsToShowGameOverScreen;
+        finalSecondsToRestart+= secondsToShowGameOverScreen;
+    }
     void Start()
     {
         onGameStart?.Invoke();
+    }
+
+    public void LoseGame()
+    {
+        onLoseGame?.Invoke();
+        Invoke("ShowGameOverScreen", secondsToShowGameOverScreen);
+    }
+
+    private void ShowGameOverScreen()
+    {
+        onShowGameOverScreen?.Invoke();
     }
 
     public void RespawnGame()
